94717d31d199030845b4f09042f3ba8539afbd4e
b3ruan <b3ruan@uwaterloo.ca>
2024-11-18 11:09:46 -0500

    a8q3

--- a8/palindrome.c
+++ a8/palindrome.c
@@ -1,12 +1,21 @@
-// #include ...
-
+#include <stdio.h>
  
 // Function to check if it is possible to rearrange the characters to form a palindrome
 void canFormPalindrome(char *str) {
-    
+    int arr[26] = {0};
+    int i = 0;
+    int num_odd = 0;
+    while(str[i]!='\0'){
+        arr[str[i]-97]++;
+        if(arr[str[i]-97]%2 != 0) num_odd++;
+        else num_odd--;
+        i++;
+    }
+    if (num_odd>1) printf("NO\n");
+    else printf("YES\n");
+    return;
 }
  
-
 //Do not change this part for testing. Do not submit it.
 int main() {
     char inputString[1001];  // you can make it bigger if you want to test longer strings
@@ -20,4 +29,3 @@ int main() {
  
     return 0;
 }
-

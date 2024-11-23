fbaf5a978e1b0e73679b0596fc5790cb63d31eb2
b3ruan <b3ruan@uwaterloo.ca>
2024-11-23 15:03:56 -0500

    a9q2

--- a9/ternary.c
+++ a9/ternary.c
@@ -1,8 +1,26 @@
-// #include ...
+#include <stdio.h>
+#include <assert.h>
+
+int help(int L[], int beginning, int end, int target, int last_occur){
+	if(beginning > end) return last_occur;
+
+	int mid1 = beginning + (end - beginning)/3;
+	int mid2 = end - (end - beginning)/3;
+
+	printf("Examining indices %d and %d\n", mid1, mid2);
+
+	if(mid2==end && L[mid2]==target) return mid2;
+
+	if(L[mid2]==target) return help(L,mid2+1,end,target,mid2);
+	else if (L[mid2]<target) return help(L,mid2+1,end,target,last_occur);
+	else if(L[mid1]==target) return help(L,mid1+1,mid2-1,target,mid1);
+	else if(L[mid1]<target) return help(L,mid1+1,mid2-1,target,last_occur);
+	else return help(L,beginning,mid1-1,target,last_occur);
+}
 
 
 int ternarylastsearch(int L[], int len, int target){
-	
+	return help(L,0,len-1,target,-1);
 }
 
 int main(void) {
@@ -22,5 +40,3 @@ int main(void) {
 
     return 0;
 }
-
-
